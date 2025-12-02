321f995982ef44e94f5a71a34ab231c08458bac9
Rohit Shaw <280404rohitshaw@gmail.com>
2025-12-02 14:02:44 +0530

    implement user account management features: change password, update details, and manage avatars

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -341,6 +341,136 @@ const getCurrentUser = asyncHandler(async(req, res) => {
     .json(new ApiResponse(200, user,
      "User CoverImage, updated successfully"))
     })
+
+    const getUserChannelProfile = asyncHandler(async(req,
+       res) => {
+        const {username} = req.params
+
+        if(!username?.trim()) {
+          throw new ApiError(400, "Username is required")
+        }
+
+       const channel =  await User.aggregate([
+        {
+          $match: {
+            username: username?.toLowerCase()
+          }
+          },
+          {
+            $lookup: {
+              from: "subscription",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers"
+            }
+          },
+           {
+            $lookup: {
+              from: "subscription",
+              localField: "_id",
+              foreignField: "subscriber",
+              as: "subscribedChannels"  // channels that user has subscribed to
+            }
+          },
+          {
+            $addFields: {
+              subscibercount: {
+                $size: "$subscribers"
+              },
+              subscribedChannelCount: {
+                $size: "$subscribedChannels"
+              },
+              isSubscribed: {
+                $cond: {
+                  if: {
+                    $in: [
+                      req.user?._id, "$subscribers".subscriber
+                    ]
+                  },
+                  then: true,
+                  else: false   
+                }
+
+              }          
+           }  
+       },
+       {
+        $project: {
+          fullName: 1,
+          username: 1,
+          subscibercount: 1,
+          subscribedChannelCount: 1,
+          isSubscribed: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 1,
+          createdAt: 1
+        }
+       }   
+    ])
+
+    if(!channel?.length) {
+      throw new ApiError(404, "Channel not found with this username")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0],
+     "User channel profile fetched successfully"))
+     })
+
+      const getWatchHistory = asyncHandler(async(req, res) => {
+        const user = await User.aggregate([
+          {
+            $match: {
+              _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+          },
+          {
+            $lookup: {
+              from: "videos",
+              localField: "watchHistory",
+              foreignField: "_id",
+              as: "watchHistory",
+              pipeline: [
+                {
+                  $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner",
+                    pipeline: [
+                      {
+                        $project: {
+                          fullName: 1,
+                          username: 1,
+                          avatar: 1
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  $addFields: {
+                    owner: {
+                      $first:"$owner" 
+                    }
+                  }
+                }
+         ] 
+        }
+        }
+        ])
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200,
+          user[0].watchHistory,
+          "Watch history fetched successfully"
+         ))
+      })
+
+
 export {
    registerUser,
    loginUser,
@@ -350,5 +480,9 @@ export {
    getCurrentUser,
    updateAccountDetails,
     updateUserAvatar,
-   updateUserCoverImage
+   updateUserCoverImage,
+    isSubscribed,
+    getUserChannelProfile,
+    getWatchHistory
+
   };
